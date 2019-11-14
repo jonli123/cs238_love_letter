68e89fbf82e8d44d4bd42f32d0900661d479b552
Jonathan Li <johnnyli@stanford.edu>
2019-11-13 20:32:44 -0800

    draft

--- game.py
+++ game.py
@@ -9,41 +9,84 @@ class Game():
         self._deck = deck
         self._players = players
         self._turn_index = 0
+        self._game_active = True
+        self._winner = -1
+        self._discards = []
 
-        total_playing = sum(
-            [1 for player in players if PlayerTools.is_playing(player)])
 
-    def do_action():
-        
+    def discard_card(card):
+        self._discards.append(card)
+
+
+    def do_action(player,action):
+        if action.card == Card.guard:
+            #guess card
+            if self._players[action.player_target].hand_card == action.guess and not self._players[action.player_target].protected:
+                self._players[action.player_target].discard()
+                self._winner = player.player
+                self._game_active = False
+
+        elif action.card == Card.priest:
+            #see other hand
+            if not self._players[action.player_target].protected:
+                player.information = self._players[action.player_target].hand_card
+
+        elif action.card == Card.baron:
+            #compare hands and eliminate
+            if not self._players[action.player_target].protected:
+                if self._players[action.player_target].hand_card > player.hand_card:
+                    self._winner = player.player
+                    self._game_active = False
+                elif self._players[action.player_target].hand_card < player.hand_card:
+                    self._winner = action.player_target
+                    self._game_active = False
+                else:
+                    player.information = self._players[action.player_target].hand_card
+
+        elif action.card == Card.handmaid:
+            #protect self
+            player.protected = True
+        elif action.card == Card.prince:
+            #Force discard
+            if not self._players[action.player_target].protected:
+                action = self._players[action.player_target].discard()
+                self._players[action.player_target].draw(deck[0])
+                deck = deck[1:]
+        elif action.card == Card.king:
+            #Trade hands
+            if not self._players[action.player_target].protected:
+                temp = self._players[action.player_target].hand_card
+                self._players[action.player_target].hand_card = player.hand_card
+                player.hand_card = temp
+        elif action.card == Card.countess:
+            #Do nothing
+            pass
+        elif action.card == Card.princess:
+            #Lose Game
+            self._winner = (player.player + 1) % 2
+            self._game_active = False
+        else:
+            throw("error")
+
 
     def do_turn(self):
-        if self._turn_index % 2 == 0:
-            self.do_action()
-        else:
-            self.
+        player = players[self._turn_index % 2]
+        player.protected = False
+        player.draw(deck[0])
+        deck = deck[1:]
+        action = player.discard()
+        self.discard_card(action.card)
+        self.do_action(player,action)
+        self._turn_index += 1
 
-
-    def is_winner(self, idx):
-        """True iff that player has won the game"""
-        if self.active():
-            return False
-        player = self._players[idx]
-        if not PlayerTools.is_playing(player):
-            return False
-        other_scores = [
-            p.hand_card > player.hand_card for p in self._players if PlayerTools.is_playing(p)]
-        return sum(other_scores) == 0
-
-    def winner(self):
-        """Return the index of the winning player. -1 if none"""
-        for idx in range(len(self._players)):
-            if self.is_winner(idx):
-                return idx
-        return -1
 
     def simulate(self):
-        winner = -1
-        while winner == -1:
+        while self._game_active:
             self.do_turn()
-            winner = self.winner()
-        return winner
+            self._game_active = len(self._deck) > 1
+        if self.winner == -1:
+            if self._players[0].hand_card > self._players[1].hand_card:
+                self.winner = 0
+            elif self._players[0].hand_card < self._players[1].hand_card:
+                self.winner = 1
+        return self._winner
