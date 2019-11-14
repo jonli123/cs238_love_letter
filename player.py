973bf85bce7ad5585f68f0ab87b38b213a846f64
Valerie Garcia <valerie.garcia01@gmail.com>
2019-11-13 21:40:32 -0800

    beefing out player state

--- player.py
+++ player.py
@@ -1,25 +1,42 @@
+import numpy as np
+
 class player():
     #player is a number?
-    def __init__(self, player, game):
+    def __init__(self, player, game, starting_hand):
         #self.my_discards = []
         #self.opponent_discards = []
         #self.pool_discards = []
         self.discards = [] 
-        self.other_hand = 0 #or it can be a number, which is the other persons hand
         self.player = player
-        self.my_hand = None
+        self.other_hand = 0 #or it can be a number, which is the other persons hand
+        self.my_hand = starting_hand
+        self.new_card = None
+        self.am_known = False
+        self.protected = False
+        self.other_protected = False
         self.win = None
-        self.game = game
 
     def get_hand(self):
         return self.my_hand
-    
-    def discard(self, card):
-        #learning!!!
-        pass
 
-    def targeted(self, action):
-        pass
+    def draw(card):
+        if(self.new_card != None):
+            return "already holding two cards {} and {}".format(self.my_hand, self.new_card) 
+        self.new_card = card
+
+    def discard():
+        self.discards.append(self.my_hand)
+        return self.my_hand
+
+    def take_turn(self):
+        self.orotected = False
+        A = self.possible_actions()
+        a = np.sample(A) #tuple of (card, target, guess)
+        if a[0] == self.my_hand:
+            self.my_hand = self.new_card
+        self.new_card = None
+        self.discards.append(a[0])
+        return a
 
     def possible_actions(self):
         pass
